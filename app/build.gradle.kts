1771410211087f1bf01bcec05a590e009170d9f9
Scarch30 <davidscarchilli@gmail.com>
2025-10-18 12:18:44 +0200

    build: update app/build.gradle.kts (deps/Play Services/whatever)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -133,6 +133,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 
     implementation("androidx.work:work-runtime-ktx:2.9.0")
 }
